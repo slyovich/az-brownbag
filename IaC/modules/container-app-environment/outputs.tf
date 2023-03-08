3678f74170b3daaa663eb532a6a0d134d03c117f
Riquen Sylvain <sylvain.riquen@elca.ch>
2023-03-08 20:53:26 +0100

    Remove duplicate

--- IaC/modules/container-app-environment/outputs.tf
+++ IaC/modules/container-app-environment/outputs.tf
@@ -6,10 +6,6 @@ output "private-link-id" {
   value = azurerm_private_link_service.container_app_environment.id
 }
 
-output "private-link-id" {
-  value = azurerm_private_link_service.container_app_environment.id
-}
-
 output "private-link-ip-address" {
   value = azurerm_private_link_service.container_app_environment.nat_ip_configuration.private_ip_address
 }
