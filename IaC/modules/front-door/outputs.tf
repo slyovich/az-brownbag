44d78915723db4da6b9cc918f171bd9dbadd241c
Riquen Sylvain <sylvain.riquen@elca.ch>
2023-03-08 21:45:06 +0100

    Remove front door origin as this must be deployed with the application

--- IaC/modules/front-door/outputs.tf
+++ IaC/modules/front-door/outputs.tf
@@ -2,6 +2,6 @@ output "front-door-id" {
   value = azurerm_cdn_frontdoor_profile.fd.id
 }
 
-output "front-door-origin-id" {
-    value = azurerm_cdn_frontdoor_origin.fd.id
-}
+# output "front-door-origin-id" {
+#     value = azurerm_cdn_frontdoor_origin.fd.id
+# }
