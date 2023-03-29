c7ce732e9a7a0d9a965caf21cc52235ea6b0950a
Riquen Sylvain <sylvain.riquen@elca.ch>
2023-03-29 20:24:08 +0200

    Add Front door origin group

--- IaC/modules/front-door/main.tf
+++ IaC/modules/front-door/main.tf
@@ -21,26 +21,26 @@ resource "azurerm_cdn_frontdoor_custom_domain" "fd" {
   }
 }
 
-# resource "azurerm_cdn_frontdoor_origin_group" "fd" {
-#   name                     = "${var.front-door-name}-origin-group"
-#   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.fd.id
-#   session_affinity_enabled = false
+resource "azurerm_cdn_frontdoor_origin_group" "fd" {
+  name                     = "${var.front-door-name}-origin-group"
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.fd.id
+  session_affinity_enabled = false
 
-#   restore_traffic_time_to_healed_or_new_endpoint_in_minutes = 10
+  restore_traffic_time_to_healed_or_new_endpoint_in_minutes = 10
 
-#   health_probe {
-#     interval_in_seconds = 100
-#     path                = "/"
-#     protocol            = "Https"
-#     request_type        = "HEAD"
-#   }
+  health_probe {
+    interval_in_seconds = 60
+    path                = "/"
+    protocol            = "Https"
+    request_type        = "HEAD"
+  }
 
-#   load_balancing {
-#     additional_latency_in_milliseconds = 50
-#     sample_size                        = 16
-#     successful_samples_required        = 10
-#   }
-# }
+  load_balancing {
+    additional_latency_in_milliseconds = 50
+    sample_size                        = 4
+    successful_samples_required        = 3
+  }
+}
 
 # resource "azurerm_cdn_frontdoor_origin" "fd" {
 #   name                          = "${var.front-door-name}-origin"
