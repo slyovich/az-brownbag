c276328ef33671a45f677da8b8edd133de45271c
Riquen Sylvain <sylvain.riquen@elca.ch>
2023-12-07 21:45:11 +0100

    Fixes redirection when user not authenticated

--- App/ACA.BlazorApp/Handlers/AuthorizedHandler.cs
+++ App/ACA.BlazorApp/Handlers/AuthorizedHandler.cs
@@ -1,4 +1,5 @@
 ﻿using ACA.BlazorApp.Providers;
+using Microsoft.AspNetCore.Components;
 using System.Net;
 
 namespace ACA.BlazorApp.Handlers
@@ -6,10 +7,14 @@ namespace ACA.BlazorApp.Handlers
     public class AuthorizedHandler : DelegatingHandler
     {
         private readonly IAuthenticationProvider _authenticationProvider;
+        private readonly NavigationManager _navigationManager;
 
-        public AuthorizedHandler(IAuthenticationProvider authenticationProvider)
+        public AuthorizedHandler(
+            IAuthenticationProvider authenticationProvider,
+            NavigationManager navigationManager)
         {
             _authenticationProvider = authenticationProvider;
+            _navigationManager = navigationManager;
         }
 
         protected override async Task<HttpResponseMessage> SendAsync(
@@ -21,6 +26,12 @@ namespace ACA.BlazorApp.Handlers
             {
                 // if server returned 401 Unauthorized, redirect to login page
                 _authenticationProvider.SignIn();
+            }
+            else if (responseMessage.StatusCode == 0)
+            {
+                // if we have a cancelled response, we refresh the page.
+                // This may happen if the backend gives a redirect response, for instance when the user is not logged in
+                _navigationManager.NavigateTo(_navigationManager.Uri, forceLoad: true);
             }
 
             return responseMessage;
--- App/ACA.BlazorApp/Program.cs
+++ App/ACA.BlazorApp/Program.cs
@@ -23,6 +23,14 @@ builder.Services.AddHttpClient(
          var navigationManager = serviceProvider.GetRequiredService<NavigationManager>();
          client.BaseAddress = new Uri(navigationManager.BaseUri);
      })
+    .ConfigurePrimaryHttpMessageHandler(handler => new HttpClientHandler
+    {
+        // When a request is made to the backend and the response redirects to the login page,
+        // the ajax call cannot redirect the user in a browser.
+        // So, we avoid having this redirection, and the httpclient cancels the redirection instead of
+        // trying to redirect and fails (Azure AD and Azure AD B2C do not support CORS)
+        AllowAutoRedirect = false,
+    })
     .AddHttpMessageHandler<AuthorizedHandler>();
 
 builder.Services.AddTransient(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("default"));
